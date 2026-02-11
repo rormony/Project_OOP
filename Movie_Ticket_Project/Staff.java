5e980b640df8e7d36f1be26d04554b913fb8f61e
Sarong Kimsorng <ounkimsorng721@gmail.com>
2026-02-11 15:41:19 +0700

    staff

--- Movie_Ticket_Project/Staff.java
+++ Movie_Ticket_Project/Staff.java
@@ -8,5 +8,49 @@
  * @author U-ser
  */
 public class Staff {
+  private int staffID;
+  private String name;
+  private double salary;
+  private String position;
+
+
+    public Staff(int staffID, String name, double salary, String position) {
+        this.staffID = staffID;
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+    }
+
+    public int getStaffID() {
+        return staffID;
+    }
+
+    public void setStaffID(int staffID) {
+        this.staffID = staffID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 
 }
